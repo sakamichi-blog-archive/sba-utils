f83c427ec6c36940ec6a66afc8868096ececf0a0
kawadah <45890038+kawadah@users.noreply.github.com>
2026-05-20 20:58:41 +0900

    Refine member field JSDoc wording

--- packages/members/common.ts
+++ packages/members/common.ts
@@ -1,14 +1,20 @@
 export interface Generation {
+  /** Internal identifier */
   key: string
+  /** Name for display. For example, "1期生" or "けやき坂46 1期生" */
   name: string
+  /** Sequence of generation within a group; lower numbers are earlier generations */
   seq: number
 }
 
 export interface Member {
+  /** Birth date in `YYYY-MM-DD` format */
   birthdate?: string
   generation: Generation
+  /** Graduation datetime in ISO 8601 format. `undefined` if not graduated. */
   graduatedAt?: string
   name: string
   nameSpaced: string
+  /** Member ID used by official website */
   uid: string
 }
