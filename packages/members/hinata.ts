d621e1b8df8ce2c6dd360b5b24621c213570b603
kawadah <45890038+kawadah@users.noreply.github.com>
2026-05-20 20:46:03 +0900

    Alphabetize member object properties

--- packages/members/hinata.ts
+++ packages/members/hinata.ts
@@ -17,351 +17,351 @@ export const generations: Generation[] = [
 export const members: Member[] = [
   // 1st generation
   {
-    uid: "1",
+    birthdate: "1995-11-10",
+    generation: generation1,
+    graduatedAt: "2020-03-31T03:00:00",
     name: "井口眞緒",
     nameSpaced: "井口 眞緒",
-    generation: generation1,
-    birthdate: "1995-11-10",
-    graduatedAt: "2020-03-31T03:00:00"
+    uid: "1"
   },
   {
-    uid: "2",
+    birthdate: "1997-12-26",
+    generation: generation1,
+    graduatedAt: "2024-01-31T03:00:00",
     name: "潮紗理菜",
     nameSpaced: "潮 紗理菜",
-    generation: generation1,
-    birthdate: "1997-12-26",
-    graduatedAt: "2024-01-31T03:00:00"
+    uid: "2"
   },
   {
-    uid: "3",
+    birthdate: "2001-12-02",
+    generation: generation1,
+    graduatedAt: "2019-08-31T03:00:00",
     name: "柿崎芽実",
     nameSpaced: "柿崎 芽実",
-    generation: generation1,
-    birthdate: "2001-12-02",
-    graduatedAt: "2019-08-31T03:00:00"
+    uid: "3"
   },
   {
-    uid: "4",
+    birthdate: "2001-05-08",
+    generation: generation1,
+    graduatedAt: "2023-08-31T03:00:00",
     name: "影山優佳",
     nameSpaced: "影山 優佳",
-    generation: generation1,
-    birthdate: "2001-05-08",
-    graduatedAt: "2023-08-31T03:00:00"
+    uid: "4"
   },
   {
-    uid: "5",
+    birthdate: "1998-02-02",
+    generation: generation1,
+    graduatedAt: "2025-01-31T03:00:00",
     name: "加藤史帆",
     nameSpaced: "加藤 史帆",
-    generation: generation1,
-    birthdate: "1998-02-02",
-    graduatedAt: "2025-01-31T03:00:00"
+    uid: "5"
   },
   {
-    uid: "6",
+    birthdate: "1997-09-05",
+    generation: generation1,
+    graduatedAt: "2024-04-30T03:00:00",
     name: "齊藤京子",
     nameSpaced: "齊藤 京子",
-    generation: generation1,
-    birthdate: "1997-09-05",
-    graduatedAt: "2024-04-30T03:00:00"
+    uid: "6"
   },
   {
-    uid: "7",
+    birthdate: "1996-01-22",
+    generation: generation1,
+    graduatedAt: "2025-05-31T03:00:00",
     name: "佐々木久美",
     nameSpaced: "佐々木 久美",
-    generation: generation1,
-    birthdate: "1996-01-22",
-    graduatedAt: "2025-05-31T03:00:00"
+    uid: "7"
   },
   {
-    uid: "8",
+    birthdate: "1999-12-17",
+    generation: generation1,
+    graduatedAt: "2025-05-31T03:00:00",
     name: "佐々木美玲",
     nameSpaced: "佐々木 美玲",
-    generation: generation1,
-    birthdate: "1999-12-17",
-    graduatedAt: "2025-05-31T03:00:00"
+    uid: "8"
   },
   {
-    uid: "9",
+    birthdate: "1998-09-20",
+    generation: generation1,
+    graduatedAt: "2025-05-31T03:00:00",
     name: "高瀬愛奈",
     nameSpaced: "高瀬 愛奈",
-    generation: generation1,
-    birthdate: "1998-09-20",
-    graduatedAt: "2025-05-31T03:00:00"
+    uid: "9"
   },
   {
-    uid: "10",
+    birthdate: "1998-11-02",
+    generation: generation1,
+    graduatedAt: "2024-08-31T03:00:00",
     name: "高本彩花",
     nameSpaced: "高本 彩花",
-    generation: generation1,
-    birthdate: "1998-11-02",
-    graduatedAt: "2024-08-31T03:00:00"
+    uid: "10"
   },
   {
-    uid: "11",
+    birthdate: "1998-08-23",
+    generation: generation1,
+    graduatedAt: "2025-02-28T03:00:00",
     name: "東村芽依",
     nameSpaced: "東村 芽依",
-    generation: generation1,
-    birthdate: "1998-08-23",
-    graduatedAt: "2025-02-28T03:00:00"
+    uid: "11"
   },
 
   // 2nd generation
   {
-    uid: "12",
+    birthdate: "2002-09-10",
+    generation: generation2,
     name: "金村美玖",
     nameSpaced: "金村 美玖",
-    generation: generation2,
-    birthdate: "2002-09-10"
+    uid: "12"
   },
   {
-    uid: "13",
+    birthdate: "2001-07-23",
+    generation: generation2,
+    graduatedAt: "2026-01-31T03:00:00",
     name: "河田陽菜",
     nameSpaced: "河田 陽菜",
-    generation: generation2,
-    birthdate: "2001-07-23",
-    graduatedAt: "2026-01-31T03:00:00"
+    uid: "13"
   },
   {
-    uid: "14",
+    birthdate: "2002-09-07",
+    generation: generation2,
     name: "小坂菜緒",
     nameSpaced: "小坂 菜緒",
-    generation: generation2,
-    birthdate: "2002-09-07"
+    uid: "14"
   },
   {
-    uid: "15",
+    birthdate: "2001-01-18",
+    generation: generation2,
+    graduatedAt: "2025-09-30T03:00:00",
     name: "富田鈴花",
     nameSpaced: "富田 鈴花",
-    generation: generation2,
-    birthdate: "2001-01-18",
-    graduatedAt: "2025-09-30T03:00:00"
+    uid: "15"
   },
   {
-    uid: "16",
+    birthdate: "2001-02-15",
+    generation: generation2,
+    graduatedAt: "2025-02-28T03:00:00",
     name: "丹生明里",
     nameSpaced: "丹生 明里",
-    generation: generation2,
-    birthdate: "2001-02-15",
-    graduatedAt: "2025-02-28T03:00:00"
+    uid: "16"
   },
   {
-    uid: "17",
+    birthdate: "2002-09-28",
+    generation: generation2,
+    graduatedAt: "2025-01-31T03:00:00",
     name: "濱岸ひより",
     nameSpaced: "濱岸 ひより",
-    generation: generation2,
-    birthdate: "2002-09-28",
-    graduatedAt: "2025-01-31T03:00:00"
+    uid: "17"
   },
   {
-    uid: "18",
+    birthdate: "1999-04-27",
+    generation: generation2,
     name: "松田好花",
     nameSpaced: "松田 好花",
-    generation: generation2,
-    birthdate: "1999-04-27"
+    uid: "18"
   },
   {
-    uid: "19",
+    birthdate: "1998-04-28",
+    generation: generation2,
+    graduatedAt: "2023-01-31T03:00:00",
     name: "宮田愛萌",
     nameSpaced: "宮田 愛萌",
-    generation: generation2,
-    birthdate: "1998-04-28",
-    graduatedAt: "2023-01-31T03:00:00"
+    uid: "19"
   },
   {
-    uid: "20",
+    birthdate: "2000-02-24",
+    generation: generation2,
+    graduatedAt: "2022-08-31T03:00:00",
     name: "渡邉美穂",
     nameSpaced: "渡邉 美穂",
-    generation: generation2,
-    birthdate: "2000-02-24",
-    graduatedAt: "2022-08-31T03:00:00"
+    uid: "20"
   },
 
   // 3rd generation
   {
-    uid: "21",
+    birthdate: "2004-04-12",
+    generation: generation3,
     name: "上村ひなの",
     nameSpaced: "上村 ひなの",
-    generation: generation3,
-    birthdate: "2004-04-12"
+    uid: "21"
   },
   {
-    uid: "22",
+    birthdate: "2003-09-27",
+    generation: generation3,
     name: "髙橋未来虹",
     nameSpaced: "髙橋 未来虹",
-    generation: generation3,
-    birthdate: "2003-09-27"
+    uid: "22"
   },
   {
-    uid: "23",
+    birthdate: "2004-02-23",
+    generation: generation3,
     name: "森本茉莉",
     nameSpaced: "森本 茉莉",
-    generation: generation3,
-    birthdate: "2004-02-23"
+    uid: "23"
   },
   {
-    uid: "24",
+    birthdate: "2004-02-23",
+    generation: generation3,
     name: "山口陽世",
     nameSpaced: "山口 陽世",
-    generation: generation3,
-    birthdate: "2004-02-23"
+    uid: "24"
   },
 
   // 4th generation
   {
-    uid: "25",
+    birthdate: "2004-08-06",
+    generation: generation4,
     name: "石塚瑶季",
     nameSpaced: "石塚 瑶季",
-    generation: generation4,
-    birthdate: "2004-08-06"
+    uid: "25"
   },
   {
-    uid: "26",
+    birthdate: "2004-08-15",
+    generation: generation4,
+    graduatedAt: "2024-02-29T03:00:00",
     name: "岸帆夏",
     nameSpaced: "岸 帆夏",
-    generation: generation4,
-    birthdate: "2004-08-15",
-    graduatedAt: "2024-02-29T03:00:00"
+    uid: "26"
   },
   {
-    uid: "27",
+    birthdate: "2004-10-03",
+    generation: generation4,
     name: "小西夏菜実",
     nameSpaced: "小西 夏菜実",
-    generation: generation4,
-    birthdate: "2004-10-03"
+    uid: "27"
   },
   {
-    uid: "28",
+    birthdate: "2005-01-15",
+    generation: generation4,
     name: "清水理央",
     nameSpaced: "清水 理央",
-    generation: generation4,
-    birthdate: "2005-01-15"
+    uid: "28"
   },
   {
-    uid: "29",
+    birthdate: "2007-02-14",
+    generation: generation4,
     name: "正源司陽子",
     nameSpaced: "正源司 陽子",
-    generation: generation4,
-    birthdate: "2007-02-14"
+    uid: "29"
   },
   {
-    uid: "30",
+    birthdate: "2006-02-20",
+    generation: generation4,
     name: "竹内希来里",
     nameSpaced: "竹内 希来里",
-    generation: generation4,
-    birthdate: "2006-02-20"
+    uid: "30"
   },
   {
-    uid: "31",
+    birthdate: "2003-07-31",
+    generation: generation4,
     name: "平尾帆夏",
     nameSpaced: "平尾 帆夏",
-    generation: generation4,
-    birthdate: "2003-07-31"
+    uid: "31"
   },
   {
-    uid: "32",
+    birthdate: "2002-04-09",
+    generation: generation4,
     name: "平岡海月",
     nameSpaced: "平岡 海月",
-    generation: generation4,
-    birthdate: "2002-04-09"
+    uid: "32"
   },
   {
-    uid: "33",
+    birthdate: "2006-08-07",
+    generation: generation4,
     name: "藤嶌果歩",
     nameSpaced: "藤嶌 果歩",
-    generation: generation4,
-    birthdate: "2006-08-07"
+    uid: "33"
   },
   {
-    uid: "34",
+    birthdate: "2005-12-31",
+    generation: generation4,
     name: "宮地すみれ",
     nameSpaced: "宮地 すみれ",
-    generation: generation4,
-    birthdate: "2005-12-31"
+    uid: "34"
   },
   {
-    uid: "35",
+    birthdate: "2003-05-20",
+    generation: generation4,
     name: "山下葉留花",
     nameSpaced: "山下 葉留花",
-    generation: generation4,
-    birthdate: "2003-05-20"
+    uid: "35"
   },
   {
-    uid: "36",
+    birthdate: "2009-02-07",
+    generation: generation4,
     name: "渡辺莉奈",
     nameSpaced: "渡辺 莉奈",
-    generation: generation4,
-    birthdate: "2009-02-07"
+    uid: "36"
   },
 
   // 5th generation
   {
-    uid: "37",
+    birthdate: "2006-12-07",
+    generation: generation5,
     name: "大田美月",
     nameSpaced: "大田 美月",
-    generation: generation5,
-    birthdate: "2006-12-07"
+    uid: "37"
   },
   {
-    uid: "38",
+    birthdate: "2007-05-05",
+    generation: generation5,
     name: "大野愛実",
     nameSpaced: "大野 愛実",
-    generation: generation5,
-    birthdate: "2007-05-05"
+    uid: "38"
   },
   {
-    uid: "39",
+    birthdate: "2006-12-26",
+    generation: generation5,
     name: "片山紗希",
     nameSpaced: "片山 紗希",
-    generation: generation5,
-    birthdate: "2006-12-26"
+    uid: "39"
   },
   {
-    uid: "40",
+    birthdate: "2006-01-23",
+    generation: generation5,
     name: "蔵盛妃那乃",
     nameSpaced: "蔵盛 妃那乃",
-    generation: generation5,
-    birthdate: "2006-01-23"
+    uid: "40"
   },
   {
-    uid: "41",
+    birthdate: "2009-03-14",
+    generation: generation5,
     name: "坂井新奈",
     nameSpaced: "坂井 新奈",
-    generation: generation5,
-    birthdate: "2009-03-14"
+    uid: "41"
   },
   {
-    uid: "42",
+    birthdate: "2006-09-10",
+    generation: generation5,
     name: "佐藤優羽",
     nameSpaced: "佐藤 優羽",
-    generation: generation5,
-    birthdate: "2006-09-10"
+    uid: "42"
   },
   {
-    uid: "43",
+    birthdate: "2006-12-26",
+    generation: generation5,
     name: "下田衣珠季",
     nameSpaced: "下田 衣珠季",
-    generation: generation5,
-    birthdate: "2006-12-26"
+    uid: "43"
   },
   {
-    uid: "44",
+    birthdate: "2007-08-01",
+    generation: generation5,
     name: "高井俐香",
     nameSpaced: "高井 俐香",
-    generation: generation5,
-    birthdate: "2007-08-01"
+    uid: "44"
   },
   {
-    uid: "45",
+    birthdate: "2004-03-27",
+    generation: generation5,
     name: "鶴崎仁香",
     nameSpaced: "鶴崎 仁香",
-    generation: generation5,
-    birthdate: "2004-03-27"
+    uid: "45"
   },
   {
-    uid: "46",
+    birthdate: "2005-06-08",
+    generation: generation5,
     name: "松尾桜",
     nameSpaced: "松尾 桜",
-    generation: generation5,
-    birthdate: "2005-06-08"
+    uid: "46"
   }
 ]
--- packages/members/sakura.ts
+++ packages/members/sakura.ts
@@ -10,345 +10,345 @@ export const generations: Generation[] = [generation1, generation2, generation3,
 export const members: Member[] = [
   // 1st generation
   {
-    uid: "03",
+    birthdate: "1997-01-05",
+    generation: generation1,
+    graduatedAt: "2025-02-28T03:00:00",
     name: "上村莉菜",
     nameSpaced: "上村 莉菜",
-    generation: generation1,
-    birthdate: "1997-01-05",
-    graduatedAt: "2025-02-28T03:00:00"
+    uid: "03"
   },
   {
-    uid: "04",
+    birthdate: "1997-10-08",
+    generation: generation1,
+    graduatedAt: "2022-10-31T03:00:00",
     name: "尾関梨香",
     nameSpaced: "尾関 梨香",
-    generation: generation1,
-    birthdate: "1997-10-08",
-    graduatedAt: "2022-10-31T03:00:00"
+    uid: "04"
   },
   {
-    uid: "06",
+    birthdate: "1998-11-15",
+    generation: generation1,
+    graduatedAt: "2025-05-31T03:00:00",
     name: "小池美波",
     nameSpaced: "小池 美波",
-    generation: generation1,
-    birthdate: "1998-11-15",
-    graduatedAt: "2025-05-31T03:00:00"
+    uid: "06"
   },
   {
-    uid: "07",
+    birthdate: "1999-10-24",
+    generation: generation1,
+    graduatedAt: "2024-02-29T03:00:00",
     name: "小林由依",
     nameSpaced: "小林 由依",
-    generation: generation1,
-    birthdate: "1999-10-24",
-    graduatedAt: "2024-02-29T03:00:00"
+    uid: "07"
   },
   {
-    uid: "08",
+    birthdate: "1998-02-16",
+    generation: generation1,
+    graduatedAt: "2025-02-14T03:00:00",
     name: "齋藤冬優花",
     nameSpaced: "齋藤 冬優花",
-    generation: generation1,
-    birthdate: "1998-02-16",
-    graduatedAt: "2025-02-14T03:00:00"
+    uid: "08"
   },
   {
-    uid: "11",
+    birthdate: "1995-11-30",
+    generation: generation1,
+    graduatedAt: "2022-12-31T03:00:00",
     name: "菅井友香",
     nameSpaced: "菅井 友香",
-    generation: generation1,
-    birthdate: "1995-11-30",
-    graduatedAt: "2022-12-31T03:00:00"
+    uid: "11"
   },
   {
-    uid: "14",
+    birthdate: "1997-07-08",
+    generation: generation1,
+    graduatedAt: "2023-12-31T03:00:00",
     name: "土生瑞穂",
     nameSpaced: "土生 瑞穂",
-    generation: generation1,
-    birthdate: "1997-07-08",
-    graduatedAt: "2023-12-31T03:00:00"
+    uid: "14"
   },
   {
-    uid: "15",
+    birthdate: "2000-05-08",
+    generation: generation1,
+    graduatedAt: "2022-08-31T03:00:00",
     name: "原田葵",
     nameSpaced: "原田 葵",
-    generation: generation1,
-    birthdate: "2000-05-08",
-    graduatedAt: "2022-08-31T03:00:00"
+    uid: "15"
   },
   {
-    uid: "18",
+    birthdate: "1997-11-13",
+    generation: generation1,
+    graduatedAt: "2021-12-31T03:00:00",
     name: "守屋茜",
     nameSpaced: "守屋 茜",
-    generation: generation1,
-    birthdate: "1997-11-13",
-    graduatedAt: "2021-12-31T03:00:00"
+    uid: "18"
   },
   {
-    uid: "20",
+    birthdate: "1995-05-16",
+    generation: generation1,
+    graduatedAt: "2021-12-31T03:00:00",
     name: "渡辺梨加",
     nameSpaced: "渡辺 梨加",
-    generation: generation1,
-    birthdate: "1995-05-16",
-    graduatedAt: "2021-12-31T03:00:00"
+    uid: "20"
   },
   {
-    uid: "21",
+    birthdate: "1998-07-28",
+    generation: generation1,
+    graduatedAt: "2022-06-30T14:59:00",
     name: "渡邉理佐",
     nameSpaced: "渡邉 理佐",
-    generation: generation1,
-    birthdate: "1998-07-28",
-    graduatedAt: "2022-06-30T14:59:00"
+    uid: "21"
   },
 
   // 2nd generation
   {
-    uid: "43",
+    birthdate: "2001-01-29",
+    generation: generation2,
+    graduatedAt: "2026-02-28T03:00:00",
     name: "井上梨名",
     nameSpaced: "井上 梨名",
-    generation: generation2,
-    birthdate: "2001-01-29",
-    graduatedAt: "2026-02-28T03:00:00"
+    uid: "43"
   },
   {
-    uid: "53",
+    birthdate: "1999-04-17",
+    generation: generation2,
     name: "遠藤光莉",
     nameSpaced: "遠藤 光莉",
-    generation: generation2,
-    birthdate: "1999-04-17"
+    uid: "53"
   },
   {
-    uid: "54",
+    birthdate: "2000-04-18",
+    generation: generation2,
     name: "大園玲",
     nameSpaced: "大園 玲",
-    generation: generation2,
-    birthdate: "2000-04-18"
+    uid: "54"
   },
   {
-    uid: "55",
+    birthdate: "1999-10-12",
+    generation: generation2,
     name: "大沼晶保",
     nameSpaced: "大沼 晶保",
-    generation: generation2,
-    birthdate: "1999-10-12"
+    uid: "55"
   },
   {
-    uid: "56",
+    birthdate: "2002-12-19",
+    generation: generation2,
     name: "幸阪茉里乃",
     nameSpaced: "幸阪 茉里乃",
-    generation: generation2,
-    birthdate: "2002-12-19"
+    uid: "56"
   },
   {
-    uid: "44",
+    birthdate: "1998-06-29",
+    generation: generation2,
+    graduatedAt: "2023-05-31T14:59:00",
     name: "関有美子",
     nameSpaced: "関 有美子",
-    generation: generation2,
-    birthdate: "1998-06-29",
-    graduatedAt: "2023-05-31T14:59:00"
+    uid: "44"
   },
   {
-    uid: "45",
+    birthdate: "2002-03-23",
+    generation: generation2,
     name: "武元唯衣",
     nameSpaced: "武元 唯衣",
-    generation: generation2,
-    birthdate: "2002-03-23"
+    uid: "45"
   },
   {
-    uid: "46",
+    birthdate: "1998-10-21",
+    generation: generation2,
     name: "田村保乃",
     nameSpaced: "田村 保乃",
-    generation: generation2,
-    birthdate: "1998-10-21"
+    uid: "46"
   },
   {
-    uid: "47",
+    birthdate: "2001-08-29",
+    generation: generation2,
     name: "藤吉夏鈴",
     nameSpaced: "藤吉 夏鈴",
-    generation: generation2,
-    birthdate: "2001-08-29"
+    uid: "47"
   },
   {
-    uid: "57",
+    birthdate: "2002-01-12",
+    generation: generation2,
     name: "増本綺良",
     nameSpaced: "増本 綺良",
-    generation: generation2,
-    birthdate: "2002-01-12"
+    uid: "57"
   },
   {
-    uid: "48",
+    birthdate: "1999-10-13",
+    generation: generation2,
     name: "松田里奈",
     nameSpaced: "松田 里奈",
-    generation: generation2,
-    birthdate: "1999-10-13"
+    uid: "48"
   },
   {
-    uid: "49",
+    birthdate: "1998-05-05",
+    generation: generation2,
+    graduatedAt: "2021-04-30T14:59:00",
     name: "松平璃子",
     nameSpaced: "松平 璃子",
-    generation: generation2,
-    birthdate: "1998-05-05",
-    graduatedAt: "2021-04-30T14:59:00"
+    uid: "49"
   },
   {
-    uid: "50",
+    birthdate: "2001-07-10",
+    generation: generation2,
     name: "森田ひかる",
     nameSpaced: "森田 ひかる",
-    generation: generation2,
-    birthdate: "2001-07-10"
+    uid: "50"
   },
   {
-    uid: "58",
+    birthdate: "2000-01-02",
+    generation: generation2,
     name: "守屋麗奈",
     nameSpaced: "守屋 麗奈",
-    generation: generation2,
-    birthdate: "2000-01-02"
+    uid: "58"
   },
   {
-    uid: "51",
+    birthdate: "2005-09-28",
+    generation: generation2,
     name: "山﨑天",
     nameSpaced: "山﨑 天",
-    generation: generation2,
-    birthdate: "2005-09-28"
+    uid: "51"
   },
 
   // 3rd generation
   {
-    uid: "59",
+    birthdate: "2002-01-13",
+    generation: generation3,
     name: "石森璃花",
     nameSpaced: "石森 璃花",
-    generation: generation3,
-    birthdate: "2002-01-13"
+    uid: "59"
   },
   {
-    uid: "60",
+    birthdate: "2006-01-09",
+    generation: generation3,
     name: "遠藤理子",
     nameSpaced: "遠藤 理子",
-    generation: generation3,
-    birthdate: "2006-01-09"
+    uid: "60"
   },
   {
-    uid: "61",
+    birthdate: "2004-07-25",
+    generation: generation3,
     name: "小田倉麗奈",
     nameSpaced: "小田倉 麗奈",
-    generation: generation3,
-    birthdate: "2004-07-25"
+    uid: "61"
   },
   {
-    uid: "62",
+    birthdate: "2005-07-07",
+    generation: generation3,
     name: "小島凪紗",
     nameSpaced: "小島 凪紗",
-    generation: generation3,
-    birthdate: "2005-07-07"
+    uid: "62"
   },
   {
-    uid: "63",
+    birthdate: "2005-04-12",
+    generation: generation3,
     name: "谷口愛季",
     nameSpaced: "谷口 愛季",
-    generation: generation3,
-    birthdate: "2005-04-12"
+    uid: "63"
   },
   {
-    uid: "64",
+    birthdate: "2003-02-17",
+    generation: generation3,
     name: "中嶋優月",
     nameSpaced: "中嶋 優月",
-    generation: generation3,
-    birthdate: "2003-02-17"
+    uid: "64"
   },
   {
-    uid: "65",
+    birthdate: "2006-11-08",
+    generation: generation3,
     name: "的野美青",
     nameSpaced: "的野 美青",
-    generation: generation3,
-    birthdate: "2006-11-08"
+    uid: "65"
   },
   {
-    uid: "66",
+    birthdate: "2006-05-09",
+    generation: generation3,
     name: "向井純葉",
     nameSpaced: "向井 純葉",
-    generation: generation3,
-    birthdate: "2006-05-09"
+    uid: "66"
   },
   {
-    uid: "67",
+    birthdate: "2004-08-18",
+    generation: generation3,
     name: "村井優",
     nameSpaced: "村井 優",
-    generation: generation3,
-    birthdate: "2004-08-18"
+    uid: "67"
   },
   {
-    uid: "68",
+    birthdate: "2005-02-15",
+    generation: generation3,
     name: "村山美羽",
     nameSpaced: "村山 美羽",
-    generation: generation3,
-    birthdate: "2005-02-15"
+    uid: "68"
   },
   {
-    uid: "69",
+    birthdate: "2005-01-22",
+    generation: generation3,
     name: "山下瞳月",
     nameSpaced: "山下 瞳月",
-    generation: generation3,
-    birthdate: "2005-01-22"
+    uid: "69"
   },
 
   // 4th generation
   {
-    uid: "70",
+    birthdate: "2004-12-22",
+    generation: generation4,
     name: "浅井恋乃未",
     nameSpaced: "浅井 恋乃未",
-    generation: generation4,
-    birthdate: "2004-12-22"
+    uid: "70"
   },
   {
-    uid: "71",
+    birthdate: "2006-03-09",
+    generation: generation4,
     name: "稲熊ひな",
     nameSpaced: "稲熊 ひな",
-    generation: generation4,
-    birthdate: "2006-03-09"
+    uid: "71"
   },
   {
-    uid: "72",
+    birthdate: "2004-01-24",
+    generation: generation4,
     name: "勝又春",
     nameSpaced: "勝又 春",
-    generation: generation4,
-    birthdate: "2004-01-24"
+    uid: "72"
   },
   {
-    uid: "73",
+    birthdate: "2006-12-01",
+    generation: generation4,
     name: "佐藤愛桜",
     nameSpaced: "佐藤 愛桜",
-    generation: generation4,
-    birthdate: "2006-12-01"
+    uid: "73"
   },
   {
-    uid: "74",
+    birthdate: "2007-09-16",
+    generation: generation4,
     name: "中川智尋",
     nameSpaced: "中川 智尋",
-    generation: generation4,
-    birthdate: "2007-09-16"
+    uid: "74"
   },
   {
-    uid: "75",
+    birthdate: "2005-02-06",
+    generation: generation4,
     name: "松本和子",
     nameSpaced: "松本 和子",
-    generation: generation4,
-    birthdate: "2005-02-06"
+    uid: "75"
   },
   {
-    uid: "76",
+    birthdate: "2006-01-24",
+    generation: generation4,
     name: "目黒陽色",
     nameSpaced: "目黒 陽色",
-    generation: generation4,
-    birthdate: "2006-01-24"
+    uid: "76"
   },
   {
-    uid: "77",
+    birthdate: "2005-09-19",
+    generation: generation4,
     name: "山川宇衣",
     nameSpaced: "山川 宇衣",
-    generation: generation4,
-    birthdate: "2005-09-19"
+    uid: "77"
   },
   {
-    uid: "78",
+    birthdate: "2008-07-20",
+    generation: generation4,
     name: "山田桃実",
     nameSpaced: "山田 桃実",
-    generation: generation4,
-    birthdate: "2008-07-20"
+    uid: "78"
   }
 ]
