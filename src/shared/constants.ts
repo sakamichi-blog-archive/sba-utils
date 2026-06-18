327ad22e0e212417f509fd726e9065b589d68e69
kawadah <45890038+kawadah@users.noreply.github.com>
2026-06-18 21:02:27 +0900

    chore: Add constants

---
+++ src/shared/constants.ts
@@ -0,0 +1,3 @@
+export const TIMEZONE_JAPAN = "Asia/Tokyo"
+export const USER_AGENT_DESKTOP =
+  "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36"
