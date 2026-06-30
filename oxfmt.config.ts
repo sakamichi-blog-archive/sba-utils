e490a69af1fa6c2736f1c25055d0218ddb2a8c94
kawadah <45890038+kawadah@users.noreply.github.com>
2026-06-30 23:40:53 +0900

    chore: Don't ignore TOML from Oxfmt

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from "oxfmt"
 
 export default defineConfig({
   arrowParens: "avoid",
-  // gitignored files are ignored by default
-  ignorePatterns: [
-    "*.toml" // Ignore because VS Code doesn't recognize TOML natively
-  ],
   semi: false,
   sortImports: true,
   trailingComma: "none"
