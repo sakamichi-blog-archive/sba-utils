210ff5493adb220f0aad55ee1a6e61391d15df8e
kawadah <45890038+kawadah@users.noreply.github.com>
2026-07-06 20:16:36 +0900

    chore: Ignore CHANGELOG.md from Oxfmt

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "oxfmt"
 
 export default defineConfig({
   arrowParens: "avoid",
+  ignore: ["CHANGELOG.md"],
   semi: false,
   sortImports: true,
   trailingComma: "none"
